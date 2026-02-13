996f4bdbf4ece6bdfc573917d5fe5ddec3970479
nadhemzini <nadhem.zini@fstsbz.u-kairouan.tn>
2026-02-13 10:51:01 +0100

    Branche B: ajout commentaire application

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // ============================================
 // IMPORTS
 // ============================================
+
+// Branche B : Application Express
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
